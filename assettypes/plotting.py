29d8b72a5d9e21e898f462216f7e8c6b6f046f9c
lbistas <lazaros.bistas@netcompany.com>
2026-04-07 15:22:19 +0300

    Add files via upload

---
+++ assettypes/plotting.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+#from mpl_toolkits.mplot3d import Axes3D
+
+N = 1000
+X = np.random.random((N, 2)) * 6 - 3
+Y = np.cos(2*X[:, 0]) + np.cos(3*X[:, 1])
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection= '3d')
+ax.scatter(X[:, 0], X[:, 1], Y)
+plt.show()
